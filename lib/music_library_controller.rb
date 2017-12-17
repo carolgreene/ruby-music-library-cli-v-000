c1ce09aa0b51f8b017b11430277406f04d3bc424
Carol Greene <carolgreene3@gmail.com>
2017-12-17 19:08:06 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -84,9 +84,9 @@ class MusicLibraryController
         puts "Which song number would you like to play?"
         input = gets.strip.to_i-1
 
-        if input => 1 && input <= list_songs.length
+        if input >= 1 && input <= list_songs.length
           choice = list_songs.songs[input]
-        end
+
         puts "Playing #{choice.name}by #{choice.artist.name}"
         end
       end
